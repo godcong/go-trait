860a5c0483fe3141a787ea7bf89bf7ff2d55d618
godcong <jumbycc@163.com>
2019-02-22 15:28:11 +0800

    update log

---
+++ go.mod
@@ -0,0 +1,9 @@
+module go-trait
+
+require (
+	github.com/godcong/elogrus v0.0.0-20190222071113-778ac7c5d538
+	github.com/olivere/elastic v6.2.16+incompatible
+	github.com/sirupsen/logrus v1.3.0
+	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd // indirect
+	gopkg.in/olivere/elastic.v5 v5.0.79 // indirect
+)
---
+++ log.go
@@ -0,0 +1,47 @@
+package go_trait
+
+import (
+	"github.com/godcong/elogrus"
+	"github.com/olivere/elastic"
+	log "github.com/sirupsen/logrus"
+)
+
+type ElasticLogOption struct {
+	ReportCaller  bool
+	Formatter     log.Formatter
+	Host          string
+	Level         log.Level
+	ClientOptions []elastic.ClientOptionFunc
+}
+
+func DefaultElasticLogOption(opt *ElasticLogOption) *ElasticLogOption {
+	if opt == nil {
+		opt = &ElasticLogOption{
+			ReportCaller: true,
+			Formatter:    &log.JSONFormatter{},
+			Host:         "localhost",
+			Level:        log.TraceLevel,
+			ClientOptions: []elastic.ClientOptionFunc{
+				elastic.SetSniff(false),
+				elastic.SetURL("http://localhost:9200"),
+			},
+		}
+	}
+}
+
+func InitElasticLog(index string, opt *ElasticLogOption) {
+	opt = DefaultElasticLogOption(opt)
+	client, err := elastic.NewClient(opt.ClientOptions...)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	t, err := elogrus.NewElasticHook(client, opt.Host, opt.Level, index)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.AddHook(t)
+
+	log.SetReportCaller(opt.ReportCaller)
+	log.SetFormatter(opt.Formatter)
+}
