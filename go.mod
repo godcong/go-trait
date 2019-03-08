c2aaaec3a01c9e0584289d5783db41a247433107
godcong <jumbycc@163.com>
2019-03-08 17:42:46 +0800

    update log

--- go.mod
+++ go.mod
@@ -1,9 +1,16 @@
 module github.com/godcong/go-trait
 
 require (
+	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
 	github.com/godcong/elogrus v0.0.0-20190222071113-778ac7c5d538
+	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
+	github.com/jonboulle/clockwork v0.1.0 // indirect
+	github.com/lestrrat-go/envload v0.0.0-20180220234015-a3eb8ddeffcc // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.2.0+incompatible
+	github.com/lestrrat-go/strftime v0.0.0-20180821113735-8b31f9c59b0f // indirect
 	github.com/olivere/elastic v6.2.16+incompatible
+	github.com/pkg/errors v0.8.1
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/sirupsen/logrus v1.3.0
-	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd // indirect
-	gopkg.in/olivere/elastic.v5 v5.0.79 // indirect
+	github.com/tebeka/strftime v0.0.0-20140926081919-3f9c7761e312 // indirect
 )
--- log.go
+++ log.go
@@ -1,9 +1,16 @@
 package trait
 
 import (
+	"fmt"
 	"github.com/godcong/elogrus"
+	"github.com/lestrrat-go/file-rotatelogs"
 	"github.com/olivere/elastic"
+	"github.com/pkg/errors"
+	"github.com/rifflock/lfshook"
 	log "github.com/sirupsen/logrus"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 type ElasticLogOption struct {
@@ -45,4 +52,91 @@ func InitElasticLog(index string, opt *ElasticLogOption) {
 
 	log.SetReportCaller(opt.ReportCaller)
 	log.SetFormatter(opt.Formatter)
+}
+
+type RotateLogOption struct {
+	Level        int           `json:"level"`
+	MaxAge       time.Duration `json:"max_age"`
+	RotationTime time.Duration `json:"rotation_time"`
+}
+
+const (
+	RotateLogAll = iota
+	RotateLogTrace
+	RotateLogDebug
+	RotateLogInfo
+	RotateLogWarn
+	RotateLogError
+	RotateLogFatal
+	RotateLogPanic
+	RotateLogOff
+)
+
+// InitRotateLogger ...
+func InitRotateLog(logPath string, opt *RotateLogOption) {
+	opt = DefaultRotateLogOption(opt)
+	dir, filename := filepath.Split(logPath)
+	_ = os.MkdirAll(dir, os.ModePerm)
+	writer, err := rotatelogs.New(
+		dir+"%Y%m%d%H%M_"+filename,
+		rotatelogs.WithLinkName(logPath),              // 生成软链，指向最新日志文件
+		rotatelogs.WithMaxAge(opt.MaxAge),             // 文件最大保存时间
+		rotatelogs.WithRotationTime(opt.RotationTime), // 日志切割时间间隔
+	)
+	if err != nil {
+		log.Errorf("config local file system logger error. %v", errors.WithStack(err))
+	}
+
+	switch opt.Level {
+	case RotateLogDebug:
+		log.SetLevel(log.DebugLevel)
+		log.SetOutput(os.Stderr)
+	case RotateLogInfo:
+		NoOutput()
+		log.SetLevel(log.InfoLevel)
+	case RotateLogWarn:
+		NoOutput()
+		log.SetLevel(log.WarnLevel)
+	case RotateLogError:
+		NoOutput()
+		log.SetLevel(log.ErrorLevel)
+	default:
+		NoOutput()
+		log.SetLevel(log.TraceLevel)
+	}
+
+	//hook := lfshook.NewHook(lfshook.WriterMap{
+	//	log.DebugLevel: writer, // 为不同级别设置不同的输出目的
+	//	log.InfoLevel:  writer,
+	//	log.WarnLevel:  writer,
+	//	log.ErrorLevel: writer,
+	//	log.FatalLevel: writer,
+	//	log.PanicLevel: writer,
+	//}, &log.JSONFormatter{})
+	hook := lfshook.NewHook(writer, &log.JSONFormatter{})
+	//hook.SetDefaultWriter(writer)
+	//hook.SetFormatter(&log.JSONFormatter{})
+	log.AddHook(hook)
+
+	log.SetReportCaller(true)
+	log.SetFormatter(&log.JSONFormatter{})
+}
+
+func DefaultRotateLogOption(opt *RotateLogOption) *RotateLogOption {
+	if opt == nil {
+		return &RotateLogOption{
+			Level:        RotateLogAll,
+			MaxAge:       30 * 24 * time.Hour, //log saved one month
+			RotationTime: 24 * time.Hour,
+		}
+	}
+	return opt
+}
+
+func NoOutput() {
+	src, err := os.OpenFile(os.DevNull, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println("err", err)
+	}
+	log.SetOutput(src)
 }
