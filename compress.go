84a6704da927520117e3aa05577274a33a3c6fb0
godcong <jumbycc@163.com>
2019-08-12 01:05:31 +0800

    add compress

---
+++ compress.go
@@ -0,0 +1,65 @@
+package trait
+
+import (
+	"bytes"
+	"compress/zlib"
+	"github.com/rakyll/statik/fs"
+	"io"
+	"os"
+	"path"
+)
+
+func CompressToFile(filename string, b []byte) error {
+	buff := bytes.NewBuffer(b)
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := zlib.NewWriter(file)
+	if _, err := io.Copy(w, buff); err != nil {
+		return err
+	}
+	w.Flush()
+	defer w.Close()
+	return nil
+}
+
+func DecompressFromFile(filename string) ([]byte, error) {
+	buff := bytes.Buffer{}
+	file, err := os.OpenFile(filename, os.O_RDWR, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	r, err := zlib.NewReader(file)
+	if err == nil {
+		if _, err := io.Copy(&buff, r); err == nil {
+			return buff.Bytes(), nil
+		}
+	}
+	defer r.Close()
+	return nil, err
+}
+
+func DecompressFromStatik(filename string) ([]byte, error) {
+	buff := bytes.Buffer{}
+	sfs, err := fs.New()
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := sfs.Open(path.Join("/", filename))
+	if err != nil {
+		return nil, err
+	}
+	r, err := zlib.NewReader(file)
+	if err == nil {
+		if _, err := io.Copy(&buff, r); err == nil {
+			return buff.Bytes(), nil
+		}
+	}
+	defer r.Close()
+	return nil, err
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/lestrrat-go/strftime v0.0.0-20180821113735-8b31f9c59b0f // indirect
 	github.com/olivere/elastic v6.2.16+incompatible
 	github.com/pkg/errors v0.8.1
+	github.com/rakyll/statik v0.1.6
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/sirupsen/logrus v1.3.0
 	github.com/tebeka/strftime v0.0.0-20140926081919-3f9c7761e312 // indirect
